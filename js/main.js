01c6262b1a0178cfefb6874c421706784c17f3b4
Kenneth Onu <kennetho.onu@gmail.com>
2023-01-16 11:49:31 +0800

    updated html, css, js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-//<button type="button" onclick="getFetch();">Get Value</button>
-
 document.querySelector('button').addEventListener('click', getFetch)
-
 const table = document.querySelector('table')
 
 function getFetch(){
-    const userInput = document.getElementById("barcode").value;
-    
+    const userInput = document.getElementById("barcode").value; 
     const url = `https://world.openfoodfacts.org/api/v0/product/${userInput}.json`
 
-        if(table.classList.add('hidden')){
-            document.location.reload()
+        if(userInput.length){
+                if(table.classList.add('hidden')){
+                    document.location.reload()
+                }else{
+                    table.classList.toggle('hidden')
+                }
         }else{
-            table.classList.toggle('hidden')
+            alert('Please enter a UPC/EAN barcode number')
+            table.classList.add('hidden')
         }
-        
 
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            //console.log(data)
-        if (data.status === 1) {
-            const item = new ProductInfo(data.product)
-            item.showInfo()
-            item.listIngredients()
-        } else {
-            alert(`Product ${userInput} not found. Please try another.`)
-        }
+                if (data.status === 1) {
+                    const item = new ProductInfo(data.product)
+                    item.showInfo()
+                    item.listIngredients()
+                } else if(userInput.length && data.status === 0){
+                    table.classList.add('hidden')
+                    alert(`Product ${userInput} not found. Please try another.`)
+                }
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -47,26 +47,21 @@ class ProductInfo {
 
     listIngredients() {
         let tableRef = document.getElementById('ingredient-table');
+        
         for(let i = 1; i < tableRef.rows.length;) {
             tableRef.deleteRow(i); //deletes the row and text of ingredient-table on index[i = 1] upon page refresh or new barcode search.
         }
+
         if (!(this.ingredients == null)) {
             for(let key in this.ingredients) {
+                
                 let newRow = tableRef.insertRow(-1);
                 let newIngredientsCell = newRow.insertCell(0);
                 let newVeganCell = newRow.insertCell(1);
                 let newVegetarianCell = newRow.insertCell(2);
-
                 let newIngredientsText = document.createTextNode(this.ingredients[key].text);
-
-                // let veganStatus = this.ingredients[key].vegan == null ? 'unknown' : this.ingredients[key].vegan 
                 let veganStatus = this.ingredients[key].vegan ? this.ingredients[key].vegan : 'unknown'
-                //if veganStatus is true (found), return VeganStatus. If falsy, return 'unknown' 
-                
-                // let vegetarianStatus = this.ingredients[key].vegetarian == null ? 'unknown' : this.ingredients[key].vegetarian
                 let vegetarianStatus = this.ingredients[key].vegetarian ? this.ingredients[key].vegetarian : 'unknown'
-                //if vegetarianStatus is true (found), return VegetarianStatus. If falsy, return 'unknown'
-
                 let newVeganText = document.createTextNode(veganStatus);
                 let newVegetarianText = document.createTextNode(vegetarianStatus);
 
@@ -74,37 +69,31 @@ class ProductInfo {
                 newVeganCell.appendChild(newVeganText);
                 newVegetarianCell.appendChild(newVegetarianText);
                 
-                //VeganCell Colors
+                //VeganStatus
                 if (veganStatus === 'yes') {
                     newVeganCell.classList.add('yes-items')
-                }
-
-                else if (veganStatus === 'no') {
+                
+                }else if (veganStatus === 'no') {
                     newVeganCell.classList.add('no-items')
-                }
-
-                else if (veganStatus === 'maybe') {
+                
+                }else if (veganStatus === 'maybe') {
                     newVeganCell.classList.add('maybe-items')
-                }
-
-                else if (veganStatus === 'unknown') {
+                
+                }else if (veganStatus === 'unknown') {
                     newVeganCell.classList.add('unknown-items')
                 }                
 
-                //VegetarianCell Colors
+                //VegetarianStatus
                 if (vegetarianStatus === 'yes') {
                     newVegetarianCell.classList.add('yes-items')
-                }
-
-                else if (vegetarianStatus === 'no') {
+                
+                }else if (vegetarianStatus === 'no') {
                     newVegetarianCell.classList.add('no-items')
-                }
-
-                else if (vegetarianStatus === 'maybe') {
+                
+                }else if (vegetarianStatus === 'maybe') {
                     newVegetarianCell.classList.add('maybe-items')
-                }
-
-                else if (vegetarianStatus === 'unknown') {
+                
+                }else if (vegetarianStatus === 'unknown') {
                     newVegetarianCell.classList.add('unknown-items')
                 }    
 
